8142c1f76f5b87bed02da86b9f428a75b03ec757
NiloufarSheyd <155656698+NiloufarSheid@users.noreply.github.com>
2025-01-05 19:56:45 +0330

    add links to react icon

--- src/components/firsthead/Firstheader.jsx
+++ src/components/firsthead/Firstheader.jsx
@@ -5,6 +5,7 @@ import EmailIcon from '@mui/icons-material/Email';
 import LocalPhoneIcon from '@mui/icons-material/LocalPhone';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import WhatsAppIcon from '@mui/icons-material/WhatsApp';
+import {Link} from '@mui/material';
 
 
 const Firstheader = () => {
@@ -13,18 +14,34 @@ const Firstheader = () => {
       <Grid item>
       <Box display="flex" gap={2} >
       
-      <Box><LinkedInIcon/></Box>
-      <Box><WhatsAppIcon/></Box>
+      <Box>
+        <Link color='#fff' fontSize="small" href='https://www.linkedin.com/in/niloufar-sheid/'>
+          <LinkedInIcon/>
+         </Link>
+      </Box>
+      
+      <Box>
+        <Link color='#fff' fontSize="small" href='https://api.whatsapp.com/send?phone=971585052435'><WhatsAppIcon/> </Link>
+      </Box>
        
        </Box>
       </Grid>
 
       <Grid item>
-      <Box display="flex" gap={2} >
+      <Box display="flex"  gap={2} >
       <Box>+971585052435</Box>
-      <Box><LocalPhoneIcon fontSize='small'/></Box>
+      <Box>
+        <Link color='#fff' fontSize="small" href='tel:00971585052435'>
+          <LocalPhoneIcon/> 
+        </Link>
+      </Box>
       <Box>info@selinacaviar.com</Box>
-      <Box><EmailIcon fontSize='small'/></Box>
+
+      <Box>
+        <Link color='#fff' fontSize="small" href="mailto:info@selinacaviar.com">
+          <EmailIcon/>
+        </Link>
+      </Box>
       
        </Box>
       </Grid>
